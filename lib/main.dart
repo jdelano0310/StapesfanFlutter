35d336fd2e12e25c9c5c8f4baa944f7fb7c34f9a
Jon Delano <jd31068@hotmail.com>
2022-07-03 05:06:11 -0400

    removed easy_sidemenu from this branch

--- lib/main.dart
+++ lib/main.dart
@@ -4,15 +4,21 @@ import 'package:flutter/material.dart';
 import 'homepage.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class MyApp extends StatefulWidget {
+  MyApp({Key? key}) : super(key: key);
 
   @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  PageController page = PageController();
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
       home: HomePage(),
     );
